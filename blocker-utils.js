c378a9d6c4101a9f65b7232366dc3a4bd7386226
santtone <sami.anttonen@trimble.com>
2017-02-09 23:59:29 +0200

    Added crash check

--- blocker-utils.js
+++ blocker-utils.js
@@ -1,6 +1,6 @@
-var blockerUtils = (function(){
+var blockerUtils = (function () {
 
-    function block(width, height, x, y, color){
+    function block(width, height, x, y, color) {
         return {
             width: width,
             height: height,
@@ -17,6 +17,23 @@ var blockerUtils = (function(){
             var topBlock = block(width, topBlockHeight, canvasWidth, 0);
             var bottomBlock = block(width, bottomBlockHeight, canvasWidth, canvasHeight - bottomBlockHeight);
             return [topBlock, bottomBlock];
+        },
+        isCrashed: function (player, blocks) {
+            var playerLeft = player.x;
+            var playerRight = player.x + player.width;
+            var playerTop = player.y;
+            var playerBottom = player.y + player.height;
+            for (var i = 0; i < blocks.length; i++) {
+                var block = blocks[i];
+                var blockLeft = block.x;
+                var blockRight = block.x + block.width;
+                var blockTop = block.y;
+                var blockBottom = block.y + block.height;
+                if ((playerBottom > blockTop) && (playerTop < blockBottom) && (playerRight > blockLeft) && (playerLeft < blockRight)) {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 
